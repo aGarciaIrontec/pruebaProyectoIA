655a05df039b847476e7eb73723ddcc90479555f
Andoni Garcia <andoni.garcia@irontec.com>
2026-04-17 13:23:38 +0200

    prueba implementacion ssr

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withEventReplay, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import { routes } from './app.routes';
@@ -16,7 +17,11 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({ scrollPositionRestoration: 'top' })
     ),
     provideHttpClient(withFetch()),
-    provideAnimations(),
+    provideClientHydration(
+      withEventReplay(),
+      withHttpTransferCacheOptions({ includePostRequests: false }),
+    ),
+    provideAnimationsAsync(),
     { provide: LOCALE_ID, useValue: 'es' },
   ],
 };
